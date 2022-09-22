371d9b091e5bfe89f78cda8bb1a3cab3ff72e0e7
Chris <chijokeogbuanya@gmail.com>
2022-09-22 10:26:26 +0000

    Contract Build

--- packages/hardhat/contracts/BlockGame.sol
+++ packages/hardhat/contracts/BlockGame.sol
@@ -148,9 +148,8 @@ contract BlockGame is ERC721, ReentrancyGuard, Ownable {
             );
         }
     }
-
-
- function mintCharacterNFT(uint256 _characterIndex) external payable {
+        
+     function mintCharacterNFT(uint256 _characterIndex) external payable {
         require(
             _characterIndex < defaultCharacters.length,
             "Character index out of bounds"
@@ -180,32 +179,197 @@ contract BlockGame is ERC721, ReentrancyGuard, Ownable {
 
 
     function claimHealth() external {
-            require(
-                nftHolders[msg.sender] != 0,
-                "You don't have a character to claim health"
-            );
-            require(
-                IERC20(blockToken).allowance(msg.sender, address(this)) >= 0.1 ether,
-                "Please approve the required token transfer before minting"
-            );
-            IERC20(blockToken).transferFrom(msg.sender, address(this), 0.1 ether);
-            uint256 tokenId = nftHolders[msg.sender];
-            CharacterAttributes memory character = nftHolderAttributes[tokenId];
-            uint256 currentTime = block.timestamp;
-            uint256 timeSinceLastRegen = currentTime - character.lastRegenTime;
+        require(
+            nftHolders[msg.sender] != 0,
+            "You don't have a character to claim health"
+        );
+        require(
+            IERC20(blockToken).allowance(msg.sender, address(this)) >= 0.1 ether,
+            "Please approve the required token transfer before minting"
+        );
+        IERC20(blockToken).transferFrom(msg.sender, address(this), 0.1 ether);
+        uint256 tokenId = nftHolders[msg.sender];
+        CharacterAttributes memory character = nftHolderAttributes[tokenId];
+        uint256 currentTime = block.timestamp;
+        uint256 timeSinceLastRegen = currentTime - character.lastRegenTime;
 
-            if (timeSinceLastRegen > regenTime) {
-                uint256 newHp = character.hp + timeSinceLastRegen.div(60);
-                if (newHp > character.maxHp) {
-                    newHp = character.maxHp;
-                }
-                character.hp = newHp;
-                character.lastRegenTime = currentTime;
-                nftHolderAttributes[tokenId] = character;
-                emit RegenCompleted(newHp);
+        if (timeSinceLastRegen > regenTime) {
+            uint256 newHp = character.hp + timeSinceLastRegen.div(60);
+            if (newHp > character.maxHp) {
+                newHp = character.maxHp;
+            }
+            character.hp = newHp;
+            character.lastRegenTime = currentTime;
+            nftHolderAttributes[tokenId] = character;
+            emit RegenCompleted(newHp);
+        }
+    }
+
+    function attackBot(uint256 attackIndex) public {
+        uint256 nftTokenIdOfPlayer = nftHolders[msg.sender];
+        CharacterAttributes storage player = nftHolderAttributes[
+            nftTokenIdOfPlayer
+        ];
+        require(player.hp > 0, "Error: character must have HP to attack bots.");
+        require(madBots.hp > 0, "Error: bot is already dead");
+        uint256 attackDamage = 0;
+        for (uint256 i = 0; i < player.attacks.length; i++) {
+            if (attackIndex == player.attacks[i]) {
+                attackDamage = allAttacks[attackIndex].attackDamage;
             }
         }
+        require(attackDamage > 0, "Error: attack must have damage.");
+        if (madBots.hp < attackDamage) {
+            madBots.hp = 0;
+        } else {
+            madBots.hp = madBots.hp - attackDamage;
+        }
 
+        if (player.hp < madBots.attackDamage) {
+            player.hp = 0;
+        } else {
+            player.hp = player.hp - madBots.attackDamage;
+        }
+        emit AttackComplete(madBots.hp, player.hp);
+    }
+
+    function buySpecialAttack(uint256 specialAttackIndex) public payable {
+        uint256 nftTokenIdOfPlayer = nftHolders[msg.sender];
+        require(
+            nftTokenIdOfPlayer > 0,
+            "Error: must have NFT to buy special attack."
+        );
+
+        CharacterAttributes storage player = nftHolderAttributes[
+            nftTokenIdOfPlayer
+        ];
+        require(
+            IERC20(blockToken).allowance(msg.sender, address(this)) >=
+                allSpecialAttacks[specialAttackIndex].price,
+            "Error: user must provide enough token to buy special attack."
+        );
+        IERC20(blockToken).transferFrom(
+            msg.sender,
+            address(this),
+            allSpecialAttacks[specialAttackIndex].price
+        );
+        player.specialAttacks.push(specialAttackIndex);
+        emit AttackComplete(madBots.hp, player.hp);
+    }
+
+
+    function checkIfUserHasNFT()
+        public
+        view
+        returns (CharacterAttributes memory)
+    {
+        uint256 userNftTokenId = nftHolders[msg.sender];
+        if (userNftTokenId > 0) {
+            return nftHolderAttributes[userNftTokenId];
+        } else {
+            CharacterAttributes memory emptyStruct;
+            return emptyStruct;
+        }
+    }
+
+    function getAllDefaultCharacters()
+        public
+        view
+        returns (CharacterAttributes[] memory)
+    {
+        return defaultCharacters;
+    }
+
+    function getAllAttacks() public view returns (AttackType[] memory) {
+        return allAttacks;
+    }
+
+    function getAllSpecialAttacks()
+        public
+        view
+        returns (SpecialAttackType[] memory)
+    {
+        return allSpecialAttacks;
+    }
+
+    function getMadBots() public view returns (MadBots memory) {
+        return madBots;
+    }
+
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[
+            _tokenId
+        ];
+        string memory strHp = Strings.toString(charAttributes.hp);
+        string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+
+        string memory specialAttacksStr = "";
+        string memory attacksStr = "";
+
+        for (uint256 i = 0; i < charAttributes.specialAttacks.length; i++) {
+            uint256 index = charAttributes.specialAttacks[i];
+            specialAttacksStr = string(
+                abi.encodePacked(
+                    specialAttacksStr,
+                    ', {"trait_type": "Special Attack - ',
+                    allSpecialAttacks[index].specialAttackName,
+                    '", "value": ',
+                    Strings.toString(
+                        allSpecialAttacks[index].specialAttackDamage
+                    ),
+                    "}"
+                )
+            );
+        }
+
+        for (uint256 i = 0; i < charAttributes.attacks.length; i++) {
+            uint256 index = charAttributes.attacks[i];
+            attacksStr = string(
+                abi.encodePacked(
+                    attacksStr,
+                    ', {"trait_type": "',
+                    allAttacks[index].attackName,
+                    '", "value": ',
+                    Strings.toString(allAttacks[index].attackDamage),
+                    "}"
+                )
+            );
+        }
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        charAttributes.name,
+                        " -- NFT #: ",
+                        Strings.toString(_tokenId),
+                        '", "description": "This is an NFT that lets people play in the Blocky NFT Game!", "image": "',
+                        charAttributes.imageURI,
+                        '", "attributes": [{"trait_type": "Health Points", "value": ',
+                        strHp,
+                        ', "max_value": ',
+                        strMaxHp,
+                        "}",
+                        specialAttacksStr,
+                        attacksStr,
+                        "]}"
+                    )
+                )
+            )
+        );
+
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+        return output;
+    }
 
     
     /********************
